2f5f2b1b7fcbd56da4d27481a352750eec2272cf
Anas-kaithakoden <anaskaithakoden@gmail.com>
2026-07-05 16:36:18 +0530

    changed with alembic

--- app/models.py
+++ app/models.py
@@ -9,6 +9,7 @@ class User(Base):
     id:Mapped[int] = mapped_column(primary_key=True)
     name:Mapped[str] = mapped_column(String(225), nullable=False)
     email: Mapped[str] = mapped_column(String(255), unique=True)
+    phone: Mapped[str] = mapped_column(String(20), nullable=True)
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda :datetime.now(timezone.utc))
     posts: Mapped[list["Post"]] = relationship( back_populates="user")
     comments: Mapped[list["Comment"]] = relationship(back_populates="user")
@@ -21,10 +22,12 @@ class Post(Base):
     title:Mapped[str] = mapped_column(Text, nullable=False)
     content:Mapped[str] = mapped_column(Text)
     published:Mapped[bool] = mapped_column(Boolean, default=False)
+    category: Mapped[str] = mapped_column(String(100),nullable=True)
     created_at:Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda :datetime.now(timezone.utc))
     user_id:Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
     user: Mapped["User"] = relationship( back_populates="posts")
     comments: Mapped[list["Comment"]] = relationship(back_populates="post")
+    
 
 
 class Comment(Base):
