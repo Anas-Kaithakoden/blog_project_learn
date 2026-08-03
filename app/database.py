c748fe734f6b8c608d880156b2bb8a23b8c371c2
Anas-kaithakoden <anaskaithakoden@gmail.com>
2026-08-03 15:47:54 +0530

    learned to compose

--- app/database.py
+++ app/database.py
@@ -8,7 +8,11 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 
-DATABASE_URL = f"postgresql+psycopg://{DB_USER}:{DB_PASSWORD}@localhost:5432/{DB_NAME}"
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    f"postgresql+psycopg://{DB_USER}:{DB_PASSWORD}@localhost:5432/{DB_NAME}",
+)
 
 engine = create_engine(DATABASE_URL)
 
