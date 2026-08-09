40409b72e02390ed3d98ddc73638821393a3ec05
Anas-kaithakoden <anaskaithakoden@gmail.com>
2026-08-09 20:14:00 +0530

    fixed alembic URL replace

--- alembic/env.py
+++ alembic/env.py
@@ -19,10 +19,10 @@ load_dotenv()
 
 database_url = config.get_main_option("sqlalchemy.url")
 
-if not database_url:
-    DATABASE_URL = os.getenv("DATABASE_URL")
-    config.set_main_option("sqlalchemy.url", DATABASE_URL)
-
+if not database_url or database_url.startswith("driver://"):
+    database_url = os.getenv("DATABASE_URL")
+    config.set_main_option("sqlalchemy.url", database_url)
+    
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
