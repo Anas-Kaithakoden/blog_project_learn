882319fced45efab418beab753bc82d4d5e0d8e7
Anas-kaithakoden <anaskaithakoden@gmail.com>
2026-08-17 00:00:33 +0530

    added corsmiddleware [skip ci]

--- app/endpoint.py
+++ app/endpoint.py
@@ -10,6 +10,16 @@ from app.models import User
 
 app = FastAPI(title="Blog API")
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 class LoginRequest(BaseModel):
     email: EmailStr
     password: str
